9d40e05ca346f5eb5af45695ec13503630bea8ed
Keks <keks@htmlacademy.ru>
2022-06-25 01:11:12 +0300

    :heavy_check_mark: Сборка #1

--- 1/js/main.js
+++ 1/js/main.js
@@ -1,16 +1,24 @@
 
-function getRandomIntInclusive(min, max) {
-    if (max > min && min >= 0) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
-    }
-    return console.log('Не пройдет, используй другие числа');
+function getRandomInteger(min, max) {
+  const maxRange = max - min + 1;
+  if (max > min && min >= 0) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * maxRange + min); //Максимум и минимум включаются
+  }
+  return 'Используй другие числа';
 }
-getRandomIntInclusive();
 
-function countSimbols () {
-  console.log(''.length)
+console.log(getRandomInteger(2, 4));
+
+function countSimbols (message, maxSimbols) {
+if (message.length >= maxSimbols) {
+  return false;
+} return true;
 }
+
+console.log(countSimbols ('1234', 10))
 
 // Источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/length
+// Другой источник: Моя подруга
